bac1ac716505e4cca4f59dca495e964800f77b7c
NickerLog <Lobov2000@gmail.com>
2022-01-19 16:43:45 +0300

    added new test should_be_login_link, added exception

--- pages/main_page.py
+++ pages/main_page.py
@@ -7,3 +7,5 @@ class MainPage(BasePage):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
 
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "a[id='login_link']"), 'login link is not presented'
